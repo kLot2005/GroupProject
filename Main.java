edee6b8af750e65e20c98d903e5a812bf8181d4e
kLot2005 <bekars.bihimbai2005@gmail.com>
2025-01-14 22:54:44 +0500

    добавил функцию toString

--- Main.java
+++ Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
